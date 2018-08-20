a616a75f8da78891d2746bd9903d8a3157064bb1
Dmitry Bolotin <dmitry.bolotin@arsenalmedia.ru>
2018-08-20 22:38:50 +1000

    add polyfills

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,19 @@
+import 'babel-polyfill';
+import 'unfetch/polyfill';
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import PromisePolyfill from 'es6-promise';
+
+PromisePolyfill.polyfill();
 
 class App extends Component {
   render() {
     return (
-      <div className="app">
-        <header className="app__header">
-          <img src={logo} className="app__logo" alt="logo" />
-          <h1 className="app__title">Welcome to React</h1>
-        </header>
-        <p className="app__intro">
-          To get started, edit <code>src/App.js</code> and save tosdfsdfoad.
-        </p>
-
+      <div className="stepper-app">
         {/* language=SCSS */}
         <style jsx>
           {`
-            .app {
+            .stepper-app {
               text-align: center;
-
-              &__logo {
-                animation: App-logo-spin infinite 20s linear;
-                height: 100px;
-              }
-
-              &__header {
-                background-color: #222;
-                height: 150px;
-                padding: 20px;
-                color: white;
-              }
-
-              &__title {
-                font-size: 1.5em;
-              }
-
-              &__intro {
-                font-size: large;
-              }
-
-              @keyframes App-logo-spin {
-                from {
-                  transform: rotate(0deg);
-                }
-                to {
-                  transform: rotate(360deg);
-                }
-              }
             }
           `}
         </style>
