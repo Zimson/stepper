62a62b05d64443dee3ac89164fce37849ef61894
Dmitry Bolotin <dmitry.bolotin@arsenalmedia.ru>
2018-08-20 23:48:52 +1000

    add postcss config

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,19 @@ class App extends Component {
   render() {
     return (
       <div className="stepper-app">
+        <h1>Hello</h1>
         {/* language=SCSS */}
         <style jsx>
           {`
             .stepper-app {
               text-align: center;
+
+              h1 {
+                display: grid;
+                transition: all 0.5s;
+                user-select: none;
+                background: linear-gradient(to bottom, white, black);
+              }
             }
           `}
         </style>
